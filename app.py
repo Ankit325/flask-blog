c267a0ee5fba6281f9e99010ec30d378a0be4034
get-conected <conected@vit.edu.in>
2022-11-12 23:50:50 +0530

    Adding blog 3 parts

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__,template_folder='templates',static_folder='static')
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', name="World")
 
 @app.route('/hello2')
 def hello_world2():
@@ -14,3 +14,12 @@ def hello_world2():
 @app.route("/redirect")
 def redirect_to_index():
     return redirect("/")
+
+@app.route("/fill-form", methods=["GET", "POST"])
+def fillForm():
+    #Method to take input
+    if request.method=="POST": 
+        name=request.form.get("name")
+        #print(name)
+        return render_template("index.html",name=name)
+    return render_template("form.html")
